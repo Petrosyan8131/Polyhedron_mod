fac796fe0287572e925fd2de82ce8fe3c22acbb1
Petrosyan8131 <107409698+Petrosyan8131@users.noreply.github.com>
2024-04-22 15:44:56 +0300

    Update run_shadow.py

--- run_shadow.py
+++ run_shadow.py
@@ -14,7 +14,7 @@ try:
         a = Polyedr(f"data/{name}.geom").edges_lenght()
         print(f'Сумма длин рёбер, удовлетворяющих условию: {a}')
         dlt_tm = time() - start_time
-        print(f"Вычисление суммы длин рёбер полиэдра '{name}' заняло {dlt_tm} сек.")
+        print(f"Вычисление суммы длин рёбер '{name}' заняло {dlt_tm} сек.")
         start_time = time()
         Polyedr(f"data/{name}.geom").draw(tk)
         delta_time = time() - start_time
